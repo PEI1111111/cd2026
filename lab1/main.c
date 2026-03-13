54355e4a7096830e014c411ffc31c58b02097382
PEI1111111 <151009469+PEI1111111@users.noreply.github.com>
2026-03-13 15:36:55 +0800

    main.c
    Refactor file reading to use 'main.c' instead of 'test.txt' and implement a linked list to store characters read from the file. Added memory management to free the linked list nodes after use.

--- lab1/main.c
+++ lab1/main.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node {
+    char ch;
+    struct Node* next;
+} Node;
+
 int main() {
-    FILE *fp = fopen("test.txt", "r"); 
-    
+    FILE *fp = fopen("main.c", "r"); 
     if (fp == NULL) {
         printf("Error: File not found.\n");
         return 1;
     }
 
+    Node *head = NULL;
+    Node *tail = NULL;
+
     int c;
     while ((c = fgetc(fp)) != EOF) {
-        unsigned char uc = (unsigned char)c;
+        Node *newNode = (Node *)malloc(sizeof(Node));
+        if (newNode == NULL) break;
+        
+        newNode->ch = (char)c;
+        newNode->next = NULL;
+
+        if (head == NULL) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            tail = newNode;
+        }
+    }
+    fclose(fp);
+
+    Node *current = head;
+    while (current != NULL) {
+        unsigned char uc = (unsigned char)current->ch;
 
         if (uc == '\n') {
             printf("'\\n' ");
@@ -20,13 +45,19 @@ int main() {
         } else if (uc == ' ') {
             printf("' ' ");
         } else if (uc == '\r') {
-            continue; 
         } else {
             printf("'%c' ", uc);
         }
+        current = current->next;
+    }
+    printf("\n");
+
+    current = head;
+    while (current != NULL) {
+        Node *temp = current;
+        current = current->next;
+        free(temp);
     }
 
-    fclose(fp);
-    printf("\n"); 
     return 0;
 }
