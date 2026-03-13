787de438a06f78eb0fbd39fabff99509b96a4f87
PEI1111111 <151009469+PEI1111111@users.noreply.github.com>
2026-03-13 15:27:55 +0800

    main.c
    Read a file and print its contents with special handling for whitespace characters.

--- lab1/main.c
+++ lab1/main.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    FILE *fp = fopen("test.txt", "r"); 
+    
+    if (fp == NULL) {
+        printf("Error: File not found.\n");
+        return 1;
+    }
+
+    int c;
+    while ((c = fgetc(fp)) != EOF) {
+        unsigned char uc = (unsigned char)c;
+
+        if (uc == '\n') {
+            printf("'\\n' ");
+        } else if (uc == '\t') {
+            printf("'\\t' ");
+        } else if (uc == ' ') {
+            printf("' ' ");
+        } else if (uc == '\r') {
+            continue; 
+        } else {
+            printf("'%c' ", uc);
+        }
+    }
+
+    fclose(fp);
+    printf("\n"); 
+    return 0;
+}
